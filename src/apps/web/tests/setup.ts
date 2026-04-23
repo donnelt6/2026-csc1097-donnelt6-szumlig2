93fa7adfc39116a9f7e8685c259111b9551403ae
Gavin Szumlicki <gavin.szumlicki2@mail.dcu.ie>
2026-04-23 19:45:57 +0100

    Mock window.matchMedia in vitest setup so AppShell tests pass under JSDOM

--- src/apps/web/tests/setup.ts
+++ src/apps/web/tests/setup.ts
@@ -6,6 +6,19 @@ if (!HTMLElement.prototype.scrollIntoView) {
   HTMLElement.prototype.scrollIntoView = () => undefined;
 }
 
+if (typeof window !== "undefined" && typeof window.matchMedia !== "function") {
+  window.matchMedia = (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => undefined,
+    removeEventListener: () => undefined,
+    addListener: () => undefined,
+    removeListener: () => undefined,
+    dispatchEvent: () => false,
+  });
+}
+
 afterEach(() => {
   cleanup();
 });
